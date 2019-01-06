1dc96ee1e7f8a5f997fd908875dc5e1e7ead6545
zcc0172@woozoom.net <zcc0172@woozoom.net>
2019-01-06 22:07:14 +0800

    2019年1月6日5 初始化

---
+++ src/main/java/com/syblackarrow/devplatform/DevplatformApplication.java
@@ -0,0 +1,14 @@
+package com.syblackarrow.devplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevplatformApplication.class, args);
+	}
+
+}
+
